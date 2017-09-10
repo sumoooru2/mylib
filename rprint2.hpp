f01a91d6c0cb6446ec7e841242d0168202242649
sumoooru2 <sumoooru2@gmail.com>
2017-09-10 19:24:07 +0900

    rptree

--- rprint2.hpp
+++ rprint2.hpp
@@ -1,15 +1,10 @@
 #pragma once
-#include<iostream>
-#include<vector>
 #include<iomanip>
-#include<queue>
-#include<unordered_set>
+#include<iostream>
+#include"rptree.hpp"
 
-#include<numeric>
-#include<map>
-#include<set>
-#include<chrono>
-#include<mutex>
+// #include<chrono>
+// #include<mutex>
 
 namespace rprint2{
 
@@ -37,19 +32,21 @@ template <class A, size_t N>
 true_type isArray(A (&)[N]);
 template <class... A>
 false_type isArray(A&&...);
+H1(isRoot,      (a->dests));
 
 #define COND(func) std::is_same<true_type, decltype(func(forward<A>(a)))>::value
 #define H2(func, cond)\
     template <class A>\
     auto func(A&& a) -> std::enable_if_t<cond, void>
-H2(rp_cout,     !COND(isArray) && COND(coutable));
+H2(rp_cout,     !COND(isArray) && !COND(isRoot) && COND(coutable));
 H2(rp_str,      !COND(coutable) && COND(strCastable));
 H2(rp_iter,     !COND(coutable) && COND(iterable));
 H2(rp_pair,     COND(isPair));
 H2(rp_array,    COND(isArray));
+H2(rp_root,    COND(isRoot));
 
 template <class F, class A>
-void printOne(ostream& out, F&& f, A&&);
+void _print(ostream& out, F&& f, A&&);
 
 //--------------------------------------------------------------------------------
 
@@ -86,11 +83,11 @@ auto print(ostream& out, F&& f, A&& a) -> decltype(rp_str(a)){
 }
 template <class F, class P>
 auto print(ostream& out, F&& f, P&& p) -> decltype(rp_pair(p)){
-    printOne(out, f, Char{'{'});
-    printOne(out, f, p.first);
-    printOne(out, f, Char{','});
-    printOne(out, f, p.second);
-    printOne(out, f, Char{'}'});
+    _print(out, f, Char{'{'}, p.first, Char{','}, p.second, Char{'}'});
+}
+template <class F, class A>
+auto print(ostream& out, F&& f, A&& a) -> decltype(rp_root(a)){
+    printTree(out, f, a);
 }
 template <class F, class A, size_t N>
 void print(ostream& out, F&& f, A (&a)[N]){
@@ -108,52 +105,50 @@ void print(ostream& out, F&& f, const char (&a)[N]){
 template <class F, class I>
 auto print_containerT(ostream& out, F&& f, I&& b, I&& e){
     for(auto& i = b; i != e; i++){
-        printOne(out, f, Char{' '});
-        printOne(out, f, *i);
+        _print(out, f, Char{' '}, *i);
     }
 }
 static int cnt = 0;
 constexpr int colorLen = 5;
-static String reset{"\e[00m"};
-static String colors[colorLen] = {{"\e[36m"}, {"\e[32m"}, {"\e[31m"}, {"\e[35m"}, {"\e[33m"}};
+static const String reset{"\e[00m"};
+static const String colors[colorLen] = {{"\e[36m"}, {"\e[32m"}, {"\e[31m"}, {"\e[35m"}, {"\e[33m"}};
 template <class F, class I>
 auto print_container(ostream& out, F&& f, I&& b, I&& e){
     if(cnt){
-        printOne(out, f, Char{'\n'});
+        _print(out, f, Char{'\n'});
         for(int i = 0; i < cnt; i++){
-            printOne(out, f, Char{'	'});
+            _print(out, f, Char{'	'});
         }
     }
-    printOne(out, f, colors[cnt]);
-    printOne(out, f, Char{'['});
-    printOne(out, f, reset);
+    _print(out, f, colors[cnt], Char{'['}, reset);
     cnt = (cnt + 1) % colorLen;
     if(b != e){
-        printOne(out, f, *b);
+        _print(out, f, *b);
         print_containerT(out, f, ++b, e);
     }
     cnt = (colorLen + cnt - 1) % colorLen;
-    printOne(out, f, colors[cnt]);
-    printOne(out, f, Char{']'});
-    printOne(out, f, reset);
+    _print(out, f, colors[cnt], Char{']'}, reset);
 }
 
 //--------------------------------------------------------------------------------
 
 template <class F, class A>
-void printOne(ostream& out, F&& f, A&& a){
+void _print(ostream& out, F&& f, A&& a){
     print(out, f, forward<A>(a));
+}
+template <class F, class A, class... As>
+void _print(ostream& out, F&& f, A&& a, As&&... as){
+    print(out, f, forward<A>(a));
+    _print(out, f, forward<As>(as)...);
 }
 
 template <class F, class A>
 void _prints(ostream& out, F&& f, A&& a){
-    printOne(out, f, forward<A>(a));
-    printOne(out, f, Char{'\n'});
+    _print(out, f, forward<A>(a), Char{'\n'});
 }
 template <class F, class A1, class... As>
 void _prints(ostream& out, F&& f, A1&& a, As&&... as){
-    printOne(out, f, forward<A1>(a));
-    printOne(out, f, Char{' '});
+    _print(out, f, forward<A1>(a), Char{' '});
     _prints(out, f, forward<As>(as)...);
 }
 
@@ -175,6 +170,10 @@ template <class... As>
 void printew(int width, As&&... as){
     _prints(std::cerr, [width](ostream& out, auto e){ out << std::setw(width) << e; }, forward<As>(as)...);
 }
+template <class... As>
+void printb(As&&... as){
+    _prints(std::cout, [](ostream& out, auto e){ out << std::bitset<64>(e); }, forward<As>(as)...);
+}
 template <class F, class... As>
 void printu(ostream& out, F&& f, As&&... as){
     _prints(out, f, forward<As>(as)...);
@@ -189,6 +188,7 @@ using rprint2::prints;
 using rprint2::printe;
 using rprint2::printw;
 using rprint2::printew;
+using rprint2::printb;
 using rprint2::printu;
 
 #define printd(...) prints(#__VA_ARGS__, __VA_ARGS__)
@@ -196,6 +196,14 @@ using rprint2::printu;
 //--------------------------------------------------------------------------------
 
 #ifdef RP_DEBUG
+#include<map>
+#include<numeric>
+#include<random>
+#include<set>
+#include<unordered_map>
+#include<vector>
+// #include<chrono>
+// #include<mutex>
 struct Elem {
     int a;
     operator std::string() {
@@ -203,9 +211,26 @@ struct Elem {
     }
 };
 
+struct Node{
+    int value = 1;
+    std::vector<Node*> dests;
+    void add(Node* n){
+        dests.push_back(n);
+    }
+    void add2(Node* n){
+        add(n);
+        n->add(this);
+    }
+    void dfs(Node* from = nullptr){
+        for(Node* d : dests){
+            if(d == from){ continue; }
+            d -> dfs(this);
+        }
+    }
+};
+
 inline void testPrint(){
     using namespace std;
-    using std::vector;
     prints(1);
     prints(1, 2, 3);
     vector<int> v = {1, 2, 3};
@@ -213,9 +238,9 @@ inline void testPrint(){
     string s = "abc";
     prints(s);
     prints("abc");
-    std::pair<int, int> p = {1, 2};
+    pair<int, int> p = {1, 2};
     prints(p);
-    std::unordered_map<int, int> um;
+    unordered_map<int, int> um;
     um[1] = 2;
     printd(um);
     Elem e;
@@ -229,21 +254,38 @@ inline void testPrint(){
     vector<int*> vp = {&v[0], &v[1], &v[2]};
     vector<vector<int>> vv(3, v);
     vector<vector<vector<int>>> vvv(3, vv);
-    std::map<int, int> mp = {{1, 2}, {4, 5}};
+    map<int, int> mp = {{1, 2}, {4, 5}};
     auto pvm = make_pair(vv, mp);
-    std::set<decltype(pvm)> spvm{pvm};
+    set<decltype(pvm)> spvm{pvm};
     prints(vvv);
     prints(mp);
     prints(pvm);
     prints(spvm);
-    std::set<std::pair<int, int>> sp = {{1, 2}};
+    set<pair<int, int>> sp = {{1, 2}};
     prints(sp);
     prints(1h, 1min, 1s, 1ms, 1us, 1ns);
     prints(1, 2, 3);
     printe(1, 2, 3);
     printw(2, 1, 2, 3);
     printew(2, 1, 2, v);
-    printu(std::cout, [](ostream& out, auto e){ out << std::setw(3) << e; }, 1, 2, 3);
+    printu(cout, [](ostream& out, auto e){ out << setw(3) << e; }, 1, 2, 3);
+
+    vector<Node> nodes(10);
+    mt19937 mt(1);
+    for(int i = 0; i < 10; i++){
+        nodes[i].value = i;
+        nodes[i].value = mt() % 10000;
+    }
+    nodes[0].add2(&nodes[1]);
+    nodes[0].add2(&nodes[2]);
+    nodes[1].add2(&nodes[3]);
+    nodes[1].add2(&nodes[4]);
+    nodes[2].add2(&nodes[5]);
+    nodes[2].add2(&nodes[6]);
+    nodes[6].add2(&nodes[7]);
+    nodes[1].add2(&nodes[8]);
+    // printTree(cout, &nodes[0]);
+    prints(&nodes[0], &nodes[1]);
 }
 
 #endif
---
+++ rptree.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#include<iostream>
+#include<queue>
+#include<unordered_set>
+#include<vector>
+
+template <class Node, class F, class V = int>
+void printTree(std::ostream& out, F&& f, Node* root, V Node::* value = &Node::value){
+    using std::vector; using std::string; using std::queue;
+    using std::shared_ptr; using std::make_shared;
+    struct N;
+    using Np = shared_ptr<N>;
+    struct N{
+        string value;
+        size_t width = 0;
+        int pos = 0;
+        vector<Np> dests;
+        N(int v): value(std::to_string(v)){ }
+        int setWidth(int p = 0){
+            pos = p;
+            for(Np n : dests){
+                width += n->setWidth(pos + width);
+            }
+            width = std::max(width, value.size() + 2);
+            return width;
+        }
+        void print(string& s, string& s2, string& s3){
+            int w = width - value.size();
+            for(uint i = 0; i < value.size(); i++){
+                s[pos + (w + 1) / 2 + i] = value[i];
+            }
+            if(dests.empty()){
+                return;
+            }
+            s2[pos + (width + 0) / 2] = '|';
+            if(dests.size() == 1){
+                s3[pos + (width + 0) / 2] = '|';
+            }else{
+                for(uint i = dests[0]->width / 2; i < width - dests.back()->width / 2; i++){
+                    s3[pos + i] = '-';
+                }
+            }
+        }
+    };
+
+    Np nroot = make_shared<N>(root->*value);
+    queue<std::pair<Node*, Np>> q;
+    q.push({root, nroot});
+    std::unordered_set<Node*> visited;
+    while(q.size()){
+        Node* n = q.front().first;
+        Np n2 = q.front().second;
+        q.pop();
+        if(visited.count(n)){ continue; }
+        visited.insert(n);
+        for(Node* d : n->dests){
+            if(visited.count(d)){ continue; }
+            Np np = make_shared<N>(d->*value);
+            n2->dests.push_back(np);
+            q.emplace(d, np);
+        }
+    }
+    int width = nroot->setWidth();
+
+    queue<Np> q2, next;
+    q2.emplace(nroot);
+    vector<string> elems, elems2, elems3;
+    out << '\n';
+    while(1){
+        string s(width, ' '), s2 = s, s3 = s;
+        while(q2.size()){
+            Np n = q2.front();
+            q2.pop();
+            n->print(s, s2, s3);
+            for(Np d : n->dests){
+                next.emplace(d);
+            }
+        }
+        f(out, s); out << '\n';
+        if(next.empty()){ break; }
+        f(out, s2); out << '\n';
+        f(out, s3); out << '\n';
+        q2.swap(next);
+    }
+}
+
+template <class Node, class V = int>
+void printTree(std::ostream& out, Node* root, V Node::* value = &Node::value){
+    printTree(out, [](std::ostream& out, auto e){ out << e; }, root, value);
+}
